fac010ab53932d4510c6394081ef74c4f75df88a
anpanthri <anpanthri@816NK12.corp.copart.com>
2017-05-17 09:53:11 -0500

    1

--- src/main/java/com/car/Car.java
+++ src/main/java/com/car/Car.java
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-//testcommit
+//testcommit 
 @Entity
 public class Car {
     @Id @GeneratedValue(strategy= GenerationType.AUTO)
