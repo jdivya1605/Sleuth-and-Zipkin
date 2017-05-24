da4af4a70383b7b6e6e2a12fb9e49c434207d4ce
dijayaprak <dijayaprak@28YRK12.corp.copart.com>
2017-05-24 10:34:29 -0500

    sleuth

--- src/main/java/com/car/CarController.java
+++ src/main/java/com/car/CarController.java
@@ -1,19 +1,37 @@
 package com.car;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.logging.Level;
 
 @RestController
 public class CarController {
 
     @Autowired
     private CarService carService;
-
-    @RequestMapping("/")
+    
+    @Autowired
+   	private RestTemplate restTemplate;
+   	
+       @Bean
+       public RestTemplate getRestTemplate() {
+           return new RestTemplate();
+       }   
+      
+	
+    
+    private static Logger log = LoggerFactory.getLogger(DemoApplication.class);
+    @RequestMapping("/home")
     public String home() {
-        return "ok!!";
+    	 log.info("Handling ***************************************  Microservice 2     ****************************");
+        return "Microservice 2!!";
     }
 
     @RequestMapping("/Cars")
@@ -38,4 +56,12 @@ public class CarController {
     public List<Car> findOwnerCar(@RequestParam int id) {
         return carService.findOwner(id);
     }
+    
+    @RequestMapping("/callhome")
+    public String callHome() {
+    	log.info("landed on Microservice 2");
+    	log.info("from Microservice 2");
+        return restTemplate.getForObject("http://localhost:8080/finaldestination/", String.class);
+    }
+    
 }
--- src/main/java/com/car/DemoApplication.java
+++ src/main/java/com/car/DemoApplication.java
@@ -1,12 +1,28 @@
 package com.car;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	
+	
+	private static Logger log = LoggerFactory.getLogger(DemoApplication.class);
+	
     public static void main(String[] args) {
+    	
+    	
+    	 log.info("Handling Main");
         SpringApplication.run(DemoApplication.class, args);
     }
+
+
+   
+
+
 }
